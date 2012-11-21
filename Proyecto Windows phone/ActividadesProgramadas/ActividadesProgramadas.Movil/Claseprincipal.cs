703a053e08d390d3fc2fdac5177aeb1752c7c2b3
dcorreal <diegof.correal@gmail.com>
2012-11-20 23:03:54 -0500

    Commit con UML y WinPhone

---
+++ Proyecto Windows phone/ActividadesProgramadas/ActividadesProgramadas.Movil/Claseprincipal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace ActividadesProgramadas.Movil
+{
+    public class Claseprincipal
+    {
+        private ObservableCollection<Actividad> lista = new ObservableCollection<Actividad>();
+
+        public ObservableCollection<Actividad> Lista
+        {
+            get { return lista; }
+            set { lista = value; }
+        }
+    }
+}
---
+++ Proyecto Windows phone/ActividadesProgramadas/ActividadesProgramadas.Movil/Convertidor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Data;
+
+namespace ActividadesProgramadas.Movil
+{
+    public class Convertidor : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            switch (value.ToString())
+            {
+                case "Reunion":
+                    return "/images/imagen.png";
+                case "Deporte":
+                    return "/images/deporte.jpg";
+                default:
+                    return "";
+            }
+    
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
